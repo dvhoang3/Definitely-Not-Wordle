20fc40878db83d1d7a75dbbd82ca660f1b5ebb89
David Hoang <dvhoang@mail.com>
2022-02-09 23:09:02 -0600

    started wordle user inputs onscreen buttons only

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
-  // Word state
-  
+  const [currentGuess, setGuess] = useState("")
+  const insertLetter = (letter) => {
+    setGuess(currentGuess + letter)
+  }
+  const deleteLetter = () => {
+    setGuess(currentGuess.slice(0, -1))
+  }
+  const submitGuess = () => {
+    setGuess("")
+  }
 
-  // handle alphabet key press
-  useEffect(() => {
-    const handleAlpha = (event) => {
-      if (event.keyCode >= 65 && event.keyCode <= 90) {
-        //String.fromCharCode(event.keyCode)
-        console.log(String.fromCharCode(event.keyCode))
-      }
-    }
-    window.addEventListener('keydown', handleAlpha);
-    return () => window.removeEventListener('keydown', handleAlpha)
-  }, [])
-
-  // handle enter key press
-  useEffect(() => {
-    const handleEnter = (event) => {
-        if (event.keyCode === 13) {
-          console.log("Enter: Clear Everything!")
-      }
-    }
-    window.addEventListener('keydown', handleEnter);
-    return () => window.removeEventListener('keydown', handleEnter)
-  }, [])
-
-  // handle backspace key press
-  useEffect(() => {
-    const handleEnter = (event) => {
-        if (event.keyCode === 8) {
-          console.log("Backspace: Clear most recent letter added!")
-      }
-    }
-    window.addEventListener('keydown', handleEnter);
-    return () => window.removeEventListener('keydown', handleEnter)
-  }, [])
-
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   return (
     <div className="App">
       <h1>Hello From React</h1>
+      <h2>{currentGuess}</h2>
+      <ul>
+        {alphabet.map(letter => <li key={letter}><button onClick={() => insertLetter(letter)}>{letter}</button></li>)}
+      </ul>
+      <button onClick={() => deleteLetter()}>Backspace</button>
+      <button onClick={() => submitGuess()}>Enter</button>
     </div>
-  );
+  )
 }
 
 export default App;
